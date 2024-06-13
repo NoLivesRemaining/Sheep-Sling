b5e2d87fc9a9be9ab09d715d1d8f49ee0581fc6b
Michael <michael.dunne@lemonchiligames.com>
2024-06-13 01:02:11 +0100

    Added and fixed tile selection

--- Bouncy World/Scripts/tileselect.gd
+++ Bouncy World/Scripts/tileselect.gd
@@ -1,12 +1,27 @@
 extends Node3D
 
-var Y_starting_position = global_position.y
+var isPickedUp = false
+var starting_position
+var prev_mouse_position
+var next_mouse_position
 
 func _on_area_3d_input_event(camera, event, position, normal, shape_idx):
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed == true:
-			global_position.y += 1
-			print("Pressed Left!", Y_starting_position, global_position.y)
-		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed == false:
-			global_position.y = Y_starting_position
-			print("Released Left!")
+			isPickedUp = true
+			prev_mouse_position = get_viewport().get_mouse_position()
+
+func _input(event):
+	if event is InputEventMouseButton and event.is_released():
+		isPickedUp = false
+		
+func _ready():
+	starting_position = position.y
+
+func _process(delta):
+	if(isPickedUp == true):
+		next_mouse_position = get_viewport().get_mouse_position()
+		position.y = (-(next_mouse_position.y - prev_mouse_position.y) * delta)
+		prev_mouse_position = next_mouse_position
+	else:
+		position.y = starting_position * delta
