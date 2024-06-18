2b958e015cc08083b6667995b3ad42487e22c2c5
Michael <michael.dunne@lemonchiligames.com>
2024-06-18 01:36:23 +0100

    Tile floor slingshot functionality refined and reworked

--- Bouncy World/Scripts/tileselect.gd
+++ Bouncy World/Scripts/tileselect.gd
@@ -1,9 +1,14 @@
 extends Node3D
 
 var isPickedUp = false
-var starting_position
+var starting_position 
 var prev_mouse_position
 var next_mouse_position
+var elasticMultiplier
+var elasticLocation
+var acceleration = 30
+var elasticAcceleration
+const roundingThreshold = 0.05
 
 func _on_area_3d_input_event(camera, event, position, normal, shape_idx):
 	if event is InputEventMouseButton:
@@ -14,14 +19,25 @@ func _on_area_3d_input_event(camera, event, position, normal, shape_idx):
 func _input(event):
 	if event is InputEventMouseButton and event.is_released():
 		isPickedUp = false
-		
+		elasticMultiplier = position.y/2
+		elasticAcceleration = acceleration * abs(elasticMultiplier)
+		elasticLocation = starting_position - elasticMultiplier
+
 func _ready():
 	starting_position = position.y
 
 func _process(delta):
-	if(isPickedUp == true):
+	if(isPickedUp):
 		next_mouse_position = get_viewport().get_mouse_position()
-		position.y = (-(next_mouse_position.y - prev_mouse_position.y) * delta)
+		position.y += (-(next_mouse_position.y - prev_mouse_position.y) * delta)
 		prev_mouse_position = next_mouse_position
-	else:
-		position.y = starting_position * delta
+	if(!isPickedUp && position.y != starting_position):
+			if(position.y == elasticLocation):
+				if(abs(elasticLocation) <= roundingThreshold):
+					elasticLocation = starting_position
+					position.y = starting_position
+				else:
+					elasticMultiplier = position.y/2
+					elasticAcceleration = acceleration * abs(elasticMultiplier)
+					elasticLocation = starting_position - elasticMultiplier
+			position.y = move_toward(position.y, (elasticLocation), (delta * elasticAcceleration))
