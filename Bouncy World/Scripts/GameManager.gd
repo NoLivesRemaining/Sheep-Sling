af52362d5be510c19f2e0f6b62544ee563ec76f1
Michael Dunne <michaelblankdunne@gmail.com>
2024-07-22 20:54:17 +0100

    Added success, win UI & game restart

--- Bouncy World/Scripts/GameManager.gd
+++ Bouncy World/Scripts/GameManager.gd
@@ -6,20 +6,25 @@ var sheepRandomiser = RandomNumberGenerator.new()
 var sheepCount
 var sheepScore = 0
 
+@onready var timer = $Timer
+
 func _ready():
 	sheepCount = sheepRandomiser.randi_range(3, 6)
+	$UI.set_process(false)
 
 func add_point():
 	sheepScore += 1
 	check_Score()
-	
 
 func spawn():
 	var new_sheep = sheep.instantiate()
 	new_sheep.global_position = $Spawnpoint.global_position
 	add_child(new_sheep)
 
-
 func check_Score():
 	if(sheepScore == sheepCount):
-		print("Win!")
+		$UI.set_process(true)
+		timer.start()
+
+func _on_timer_timeout():
+	get_tree().reload_current_scene()
