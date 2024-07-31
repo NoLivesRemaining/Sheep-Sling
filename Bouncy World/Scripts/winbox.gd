c245bf9126954ee75e986b346e41d33ed575a570
Michael Dunne <michaelblankdunne@gmail.com>
2024-07-31 16:47:22 +0100

    Added sheep saving in coral

--- Bouncy World/Scripts/winbox.gd
+++ Bouncy World/Scripts/winbox.gd
@@ -1,8 +1,15 @@
 extends Node3D
 
 @onready var game_manager = %GameManager
+@export var sheepSpawnArray: Array[Node3D]
+var sheepTracker = 0
 
+func _ready():
+	for index in sheepSpawnArray.size():
+		sheepSpawnArray[index].set_visible(false)
 
 func _on_win_area_body_entered(body):
 	game_manager.add_point()
+	sheepSpawnArray[sheepTracker].set_visible(true)
+	sheepTracker += 1
 	body.queue_free()
